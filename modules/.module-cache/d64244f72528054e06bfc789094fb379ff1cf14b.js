3589840d82e007a38b2920408903db1cbd45a9ad
Thomas Britton <tom.britton@wholefoods.com>
2015-02-25 13:23:40 -0600

    Some code cleanup, deleting node_modules, adding them to package.json, deleting cache directory, adding them to .gitignore

--- modules/.module-cache/d64244f72528054e06bfc789094fb379ff1cf14b.js
+++
@@ -1,8 +0,0 @@
-global.document= window.document;
-global.navigator = window.navigator;
-var $ = require('jquery');
-var Q = require('Q');
-var MessageView = require('../modules/messageView.js');
-var mustache = require('mustache');
-var dbHandler = new window.dbHandler();
-
--- modules/.module-cache/e2eeb4898b1b5c66f8303ffcfabb7eed9ea648d1.js
+++
@@ -1,6 +0,0 @@
-var $ = require('jquery');
-var Q = require('Q');
-var MessageView = require('../modules/messageView.js');
-var mustache = require('mustache');
-var dbHandler = new window.dbHandler();
-
