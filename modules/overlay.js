8d4b0788bfec6542a72268ef81ed2c809ba39246
Chris Kirk <cperryk@gmail.com>
2015-08-01 16:15:58 -0400

    Added ES6 features to Overlay

--- modules/overlay.js
+++ modules/overlay.js
@@ -1,39 +1,40 @@
+// jshint esnext: true
+
 var $ = require('jquery');
 var EventEmitter = require('events').EventEmitter;
-var util = require('util');
 
-function Overlay(conf){
-	var self = this;
-	this.conf = conf;
-	this.$shade = $('<div>')
-		.addClass('overlay')
-		.appendTo('body')
-		.fadeTo(100,1);
-	this.$c = $('<div>')
-		.addClass('inner')
-		.appendTo(this.$shade);
-	$('<div>')
-		.addClass('btn_ex')
-		.html('X')
-		.appendTo(this.$c)
-		.click(function(){
-			self.close();
+class Overlay extends EventEmitter{
+	constructor(conf){
+		var self = this;
+		this.conf = conf;
+		this.$shade = $('<div>')
+			.addClass('overlay')
+			.appendTo('body')
+			.fadeTo(100,1);
+		this.$c = $('<div>')
+			.addClass('inner')
+			.appendTo(this.$shade);
+		$('<div>')
+			.addClass('btn_ex')
+			.html('X')
+			.appendTo(this.$c)
+			.click(function(){
+				self.close();
+			});
+		$(window).on('keydown.overlay', function(e){
+			if(e.keyCode === 27){ // escape key
+				self.close();
+			}
 		});
-	$(window).on('keydown.overlay', function(e){
-		if(e.keyCode === 27){ // escape key
-			self.close();
-		}
-	});
+	}
+	close(){
+		$(window).unbind('keydown.overlay');
+		this.$shade.remove();
+		this.emit('close');
+	}
+	getContainer(){
+		return this.$c;
+	}
 }
 
-util.inherits(Overlay, EventEmitter);
-
-Overlay.prototype.close = function(){
-	$(window).unbind('keydown.overlay');
-	this.$shade.remove();
-	this.emit('close');
-};
-Overlay.prototype.getContainer = function(){
-	return this.$c;
-};
 module.exports = Overlay;
